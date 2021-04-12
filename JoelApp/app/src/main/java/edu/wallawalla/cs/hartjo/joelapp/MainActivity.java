af752f929855569df942e9ead04b87f4bf579a1c
Joel Hartman <65033855+joelhart@users.noreply.github.com>
2021-04-12 13:44:28 -0700

    Project Assignment 2

--- JoelApp/app/src/main/java/edu/wallawalla/cs/hartjo/joelapp/MainActivity.java
+++ JoelApp/app/src/main/java/edu/wallawalla/cs/hartjo/joelapp/MainActivity.java
@@ -3,6 +3,12 @@ package edu.wallawalla.cs.hartjo.joelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                // rating is 1 to numStars
+                TextView text = (TextView)findViewById(R.id.textView2);
+                if (rating < 4) {
+                    text.setText("Rate Better Please");
+                } else {
+                    text.setText("Thats Better!");
+                }
+            }
+        });
     }
+
+    public void onToggleClicked(View view) {
+        // Is the toggle on?
+        boolean on = ((ToggleButton) view).isChecked();
+
+        if (on) {
+            // Enable
+        } else {
+            // Disable
+        }
+    }
+
+
+
 }
